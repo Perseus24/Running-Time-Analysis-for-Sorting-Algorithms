e435e3df2411a1831532c319f04c6e36f74e9882
Perseus24 <105843665+Perseus24@users.noreply.github.com>
2023-03-20 22:46:28 +0800

    Update CS111_project.c

--- CS111_project.c
+++ CS111_project.c
@@ -7,6 +7,8 @@
 #define CLOCKS_PER_MS (CLOCKS_PER_SEC/1000)
 
 void insertionSort(unsigned long int (*sortedArr)[6], int N);
+void bubbleSort(unsigned long int (*sortedArr)[6], int N);
+void selectionSort(unsigned long int (*sortedArr)[6], int N);
 
 int main(){
 	
@@ -15,7 +17,7 @@ int main(){
 	int r;
 	clock_t start, end;
 	double cpu_time_used;
-	unsigned long int sortedArr[N][6];
+	unsigned long int sortedArr[N][6];          
 	double runningTime[6];
 	FILE output;
 											//generating an unsorted array
@@ -30,7 +32,6 @@ int main(){
 	srand(time(0));
 	for(i=0; i<N; i++){
 		unsortedArr[i] = (rand()% max + 1);
-		//printf(" %lu,", unsortedArr[i]);
 	}
 	
 							//copying unsorted array to the 2D array
@@ -46,16 +47,22 @@ int main(){
 					insertionSort(sortedArr, N);
 					end = clock();
 					break;
-					
+			case 1: start = clock();
+					bubbleSort(sortedArr, N);
+					end = clock();
+					break;
+			
+			case 2: start = clock();
+					selectionSort(sortedArr, N);
+					end = clock();
+					break;
 		}
-		cpu_time_used = ((double) (end - start))/ CLOCKS_PER_SEC;
+		cpu_time_used = ((double) (end - start))/ CLOCKS_PER_SEC; 
 		runningTime[i] = cpu_time_used;
 		printf("\n\n||The running time is %.10f", cpu_time_used);
+
 		
 	}
-	
-	
-	//	printf("\n\n||The running time is %.10f", cpu_time_used);
 
 	return 0;
 	
@@ -84,11 +91,46 @@ void insertionSort(unsigned long int (*sortedArr)[6], int N){
 	
 }
 
-void bubbleSort(){
+void bubbleSort(unsigned long int (*sortedArr)[6], int N){
+	
+	int i, n, temp;
+	
+	for (i = 0; i<N; i++)
+    {
+        for (n = 0; n < N - 1; n++)
+        {
+            if  (sortedArr[n][1] >  sortedArr[n+1][1])
+            {
+                temp = sortedArr[n][1];
+                sortedArr[n][1] = sortedArr[n+1][1];
+                sortedArr[n+1][1] = temp;
+            }
+        }
+    }
 	
 }
 
-void selectionSort(){
+void selectionSort(unsigned long int (*sortedArr)[6], int N){
+	
+	int i, position, swap, length;
+	for (i = 0; i<N-1; i++)
+    {
+        position = i;
+        for (length = i+1; length<N; length++)
+        {
+        	
+            if (sortedArr[position][2] < sortedArr[length][2])
+            {
+                position = length;
+            }
+        }
+        if (position != i)
+        {
+            swap = sortedArr[i][2];
+            sortedArr[i][2] = sortedArr[position][2];
+            sortedArr[position][2] = swap;
+        }   
+    }
 	
 }
 
