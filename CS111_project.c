335f61f6c67aaf0ba7dc3745fe861dc37729f208
Perseus24 <105843665+Perseus24@users.noreply.github.com>
2023-03-20 22:04:13 +0800

    Update CS111_project.c

--- CS111_project.c
+++ CS111_project.c
@@ -1,33 +1,63 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<time.h>
 #include<limits.h>
 
-void insertionSort(unsigned long int unsortedArr[], int N);
 
-void main(){
+#define CLOCKS_PER_MS (CLOCKS_PER_SEC/1000)
+
+void insertionSort(unsigned long int (*sortedArr)[6], int N);
+
+int main(){
 	
 	int N;							//Number of Integers
-	
-	N=100000;
-	
-							//generating an unsorted array
-	time_t timer;
+	N=10000;
+	int r;
+	clock_t start, end;
+	double cpu_time_used;
+	unsigned long int sortedArr[N][6];
+	double runningTime[6];
+	FILE output;
+											//generating an unsorted array
+
 	int i;
+	int j;
 	unsigned long int max = ULONG_MAX;
 	unsigned long int unsortedArr[N];
 	//As of 3/15/2023, the maximum input is 500,000 without the use of static
 	//unsigned long static int v[1000000] should be used for 1 million integers
 	
-	srand((unsigned long int)(time(NULL)));
-	
+	srand(time(0));
 	for(i=0; i<N; i++){
 		unsortedArr[i] = (rand()% max + 1);
-		printf(" %lu,", unsortedArr[i]);
+		//printf(" %lu,", unsortedArr[i]);
+	}
+	
+							//copying unsorted array to the 2D array
+	for(i=0; i<N; i++){
+		for(j=0; j<6; j++){
+			sortedArr[i][j] = unsortedArr[i];
+		}
+	}
+	
+	for(i=0; i<6; i++){
+		switch(i){
+			case 0: start = clock();
+					insertionSort(sortedArr, N);
+					end = clock();
+					break;
+					
+		}
+		cpu_time_used = ((double) (end - start))/ CLOCKS_PER_SEC;
+		runningTime[i] = cpu_time_used;
+		printf("\n\n||The running time is %.10f", cpu_time_used);
+		
 	}
 	
 	
-	insertionSort(unsortedArr, N);
-	
+	//	printf("\n\n||The running time is %.10f", cpu_time_used);
+
+	return 0;
 	
 }
 
@@ -36,36 +66,20 @@ void generateSorted(){
 	
 }
 
-void insertionSort(unsigned long int unsortedArr[], int N){
-	
-	clock_t start, end;
-	double cpu_time_used;
-	
-	start = clock();
+void insertionSort(unsigned long int (*sortedArr)[6], int N){
 	
 	int i, key, initial, count=0;
     for(i=1; i<N; i++)
     {
-        initial = unsortedArr[i];
+        initial = sortedArr[i][0];
         key = i-1;
-        while (key >=0 && unsortedArr[key] >  initial)
+        while (key >=0 && sortedArr[key][0] >  initial)
         {
-            unsortedArr[key+1] = unsortedArr[key];
+            sortedArr[key+1][0] = sortedArr[key][0];
             key = key - 1;
         }
-        unsortedArr[key+1] = initial;
+        sortedArr[key+1][0] = initial;
     }
-    
-    end = clock();
-    cpu_time_used = ((double) (end - start))/ CLOCKS_PER_SEC;
-    printf("\t\t\nUSING INSERTION SORT: \n");
-    for(i=0; i<N; i++){
-    	printf("\t> %d, ", unsortedArr[i]);
-	}
-	
-	
-	
-	printf("\n\n||The running time is %.10lf", cpu_time_used);
     
 	
 }
